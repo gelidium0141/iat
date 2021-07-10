ba70d184c3ded0939da7ba99d77a7a882d103f27
gelidium0141 <86865696+gelidium0141@users.noreply.github.com>
2021-07-10 15:23:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -369,6 +369,7 @@ timeline.push({
   fullscreen_mode: true
 });
 timeline.push(name_get);
+timeline.push(name_options_get);
 timeline.push(instructions_block1);
 timeline.push(trial_block1);
 timeline.push(instructions_block2);
