6fae6926ef19b445fd8e5db5bc64d807886e6a5d
gelidium0141 <86865696+gelidium0141@users.noreply.github.com>
2021-09-30 17:17:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ var trial_block1 = {
 
 var instructions_block2 = {
   type: 'html-keyboard-response',
-  stimulus:"<img src='" + repo_site + "experiment/iat_cor.png' width='60%'></img>"+"<p style = text-align: center'><strong>ここからが本番です</strong><br>" +"親友もしくは自分の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "先ほど入力した名前以外の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
+  stimulus:"<img src='" + repo_site + "experiment/iat_cor.png' width='60%'></img>"+"<p style = text-align: center'><strong>ここからが本番です</strong><br>" +"親友もしくは自分の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "先ほど入力した名前以外の名前が出てきたときは見知らぬ人として右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
  "間違えた場合は、赤い文字でxが表示されます。正しいキーを押しなおしてください。<br>" +
   "できるだけ早く、正確にキーを押してください。<br><br> " +
   "準備ができたらスペースキーを押して始めて下さい</p>",
@@ -220,7 +220,7 @@ var trial_block3 = {
 
 var instructions_block4 = {
   type: 'html-keyboard-response',
-  stimulus: "<img src='" + repo_site + "experiment/chamge.png' width='60%'></img>"+"<p style = text-align: center'><strong>先ほどとは自己の位置が逆になっています</strong><br>"+"親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "先ほど入力した名前以外の名前もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
+  stimulus: "<img src='" + repo_site + "experiment/chamge.png' width='60%'></img>"+"<p style = text-align: center'><strong>先ほどとは自己の位置が逆になっています</strong><br>"+"親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "見知らぬ人もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
  "間違えた場合は、赤い文字でxが表示されます。正しいキーを押しなおしてください。<br>" +
   "できるだけ早く、正確にキーを押してください。<br><br> " +
   "準備ができたらスペースキーを押して始めて下さい</p>",
@@ -281,7 +281,7 @@ var trial_block4 = {
 
 var instructions_block5 = {
   type: 'html-keyboard-response',
-  stimulus:"<img src='" + repo_site + "experiment/iat_inc.png' width='60%'></img>"+"<p style = text-align: center'><strong>これは先ほどと同じ課題です。</strong><br>" + "親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "先ほど入力した名前以外の名前もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
+  stimulus:"<img src='" + repo_site + "experiment/iat_inc.png' width='60%'></img>"+"<p style = text-align: center'><strong>これは先ほどと同じ課題です。</strong><br>" + "親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "見知らぬ人もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
   "間違えた場合は、赤い文字でxが表示されます。正しいキーを押しなおしてください。<br>" +
   "できるだけ早く、正確にキーを押してください。<br><br> " +
   "準備ができたらスペースキーを押して始めて下さい</div>",
