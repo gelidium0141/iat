d516dd20a2a067cc3c0a0611f7c465c107f0d82d
gelidium0141 <86865696+gelidium0141@users.noreply.github.com>
2021-10-02 11:45:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -220,7 +220,7 @@ var trial_block3 = {
 
 var instructions_block4 = {
   type: 'html-keyboard-response',
-  stimulus: "<img src='" + repo_site + "experiment/chamge.png' width='60%'></img>"+"<p style = text-align: center'><strong>先ほどとは自己の位置が逆になっています</strong><br>"+"親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "見知らぬ人もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
+  stimulus: "<img src='" + repo_site + "experiment/chamge.png' width='60%'></img>"+"<p style = text-align: center'><strong>先ほどとは自分の位置が逆になっています</strong><br>"+"親友の名前が出てきたときは左手の中指で<strong>E</strong>キーを押してください。 <br>" + "見知らぬ人もしくは自分の名前が出てきたときは右手の中指で<strong>I</strong>キーを押してください。<br><br>" + "もし" +
  "間違えた場合は、赤い文字でxが表示されます。正しいキーを押しなおしてください。<br>" +
   "できるだけ早く、正確にキーを押してください。<br><br> " +
   "準備ができたらスペースキーを押して始めて下さい</p>",
