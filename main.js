ad276ce6ae245c2c925343889012ccb1745d56c6
gelidium0141 <86865696+gelidium0141@users.noreply.github.com>
2021-11-11 19:15:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ var fullscreen_start ={
 var experimentstart = {
   type: 'instructions',
   pages: ["<p style = text-align: center'><p>これから次の課題で利用するため、あなたとあなたが先ほど思い浮かべた友人のプロフィールについて回答していただきます</p>"+"<p>ここで収集した情報については、次の課題でのみ利用し、研究以外の目的では利用しません</p>"
-  　　　　　+"次のページからは下図のように、あなたとあなたの友人それぞれの<b>姓・名<b>を回答していただきます</p>"+"<img src='" + repo_site + "experiment/name_get.png' width='55%'></img>"+"<p>姓と名はそれぞれ漢字で回答してください</p>"
+  　　　　　+"次のページからは下図のように、あなたとあなたの友人それぞれの<b>姓・名<b>を回答していただきます</p><br>"+"<img src='" + repo_site + "experiment/name_get.png' width='55%'></img>"+"<p>姓と名はそれぞれ漢字で回答してください</p>"
            +"<p>準備ができたら次へを押して回答して下さい</p>"
   ],
   allow_backward: false,
@@ -126,7 +126,7 @@ var oname_get = {
 //カテゴリー取得の説明
 var experimentstart2 = {
   type: 'instructions',
-  pages: ["<img src='" + repo_site + "experiment/cate_get.png' width='70%'></img>"+"<p style = text-align: center'>次のページからは上図のように、<b>あなたとあなたが先ほど思い浮かべた友人それぞれのプロフィール</b>を回答していただきます</p>"+"<p>もし、<b>質問に対する回答がわからない場合</b>は回答欄上部の<b>ボックスにチェック</b>をしてください</p><br>"+"<p>準備ができたら次へを押して回答して下さい</p>"
+  pages: ["<img src='" + repo_site + "experiment/cate_get.png' width='70%'></img>"+"<br><p style = text-align: center'>次のページからは上図のように、<b>あなたとあなたが先ほど思い浮かべた友人それぞれのプロフィール</b>を回答していただきます</p>"+"<p>もし、<b>質問に対する回答がわからない場合</b>は回答欄上部の<b>ボックスにチェック</b>をしてください</p><br>"+"<p>準備ができたら次へを押して回答して下さい</p>"
   ],
   allow_backward: false,
   show_clickable_nav: true,
